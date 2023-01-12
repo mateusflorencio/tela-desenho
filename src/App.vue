18c15967f8bd37a441bc2292b37d8e6dfe1bd967
Mateus Florencio <mateus.florencio@invent-corp.com>
2023-01-12 13:05:08 -0300

    adicionando opções

--- src/App.vue
+++ src/App.vue
@@ -1,222 +1,142 @@
 <template>
-  <div class="modal" v-if="showCard" :style="showCard ? 'display: flex;' : 'display: none' ">
-    <transition name="fade">
-      <div class="suspended-card">
-        <button @click="handleOption1">Sorter</button>
-        <button @click="handleOption2">Order Start 2</button>
-        <button @click="handleOption3">Esteira</button>
-        <button @click="handleOption4">Rejeito</button>
-        <button @click="handleOption5">Limpar</button>
-      </div>
-    </transition>
-
-
-    <transition name="fade" class="modalInput" v-if="showCardInput">
-      <div class="suspended-card">
-        <label for="">identificador plc</label>
-        <label for="">nome no banco de dados</label>
-        <button @click="handleOption6">Salvar</button>
-
-      </div>
-    </transition>
-  </div>
   <main>
     <table>
-      <tr v-for="(row, i) in rows" :key="i">
-        <td v-for="(col, j) in cols" :key="j" :id="[i, j].join('')" @click="handleClickInTable"></td>
-      </tr>
+      <tbody>
+        <tr v-for="(row, i) in matrix" :key="i">
+          <td v-for="(col, j) in row" :class="col.class" :key="j" :id="`${i}-${j}`" @click="handleClickInTable">
+          </td>
+        </tr>
+      </tbody>
     </table>
   </main>
+
+  <nav>
+    <div>
+      <button @click="() => classe = 'order'" class="order">Order</button>
+      <button @click="() => classe = 'sorter'" class="sorter">Sorter</button>
+      <button @click="() => classe = 'esteira'" class="esteira">esteira</button>
+      <button @click="() => classe = 'rejeito'" class="rejeito">rejeito</button>
+      <button @click="() => classe = 'pdv'" class="pdv">pdv</button>
+      <button @click="limpar">Limpar</button>
+      <button>Salvar</button>
+    </div>
+  </nav>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
-const showCard = ref(false)
-const showCardInput = ref(false)
-const height = window.screen.height - 60
-const width = window.screen.width - 60
-const rows = Array.from({ length: height / 60 }, (_, i) => i)
-const cols = Array.from({ length: width / 60 }, (_, i) => i)
-const id = ref('')
+const classe = ref('')
+const matrix = ref([])
+const size = 50
+const height = window.screen.height - size
+const width = window.screen.width - size
+const rows = Array.from({ length: height / size }, (_, i) => i)
+const cols = Array.from({ length: width / size }, (_, i) => i)
+matrix.value = rows.map(() => cols.map(() => ({ class: '' })))
 
 const handleClickInTable = (e) => {
-  showCard.value = !showCard.value
-  id.value = e.target.id
+  const [row, col] = e.target.id.split('-').map((item) => Number(item))
+  if (matrix.value[row][col].class) matrix.value[row][col].class = ''
+  else matrix.value[row][col].class = classe.value
 }
 
-const handleOption1 = () => {
-  showCardInput.value = true
-  document.getElementById(id.value).classList.remove(['sorter', 'esteira', 'rejeito'])
-  document.getElementById(id.value).className += ' sorter'
-}
-
-const handleOption2 = () => {
-  document.getElementById(id.value).classList.remove(['sorter', 'esteira', 'rejeito'])
-  document.getElementById(id.value).className += ' order'
-  showCardInput.value = true
-}
-
-const handleOption3 = () => {
-  document.getElementById(id.value).classList.remove(['sorter', 'order', 'rejeito'])
-  document.getElementById(id.value).classList.add('esteira')
-  showCard.value = false
-}
-
-const handleOption4 = () => {
-  document.getElementById(id.value).classList.remove(['sorter', 'order', 'esteira'])
-  document.getElementById(id.value).classList.add('rejeito')
-  showCardInput.value = true
-}
-
-const handleOption5 = () => {
-  document.getElementById(id.value).classList.remove(['sorter', 'order', 'esteira', 'rejeito'])
-  showCard.value = false
-}
-
-const handleOption6 = () => {
-  showCardInput.value = false
-  showCard.value = false
+const limpar = () => {
+  matrix.value = rows.map(() => cols.map(() => ({ class: '' })))
 }
 </script>
 
 <style scoped lang="scss">
-label {
-  display: inline-block;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-input {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-
-button {
-  background-color: #ffc500;
-  border: none;
-  border-radius: 10px;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  min-width: 200px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #bf9200;
-  }
-}
-
-
-
-.modal {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 1;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  .suspended-card {
-    position: absolute;
-    background-color: rgb(145, 145, 145);
-    padding: 1rem;
-    border-radius: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-
-  }
-  .modalInput {
-  @extend .modal;
-  z-index: 2;
-  width: 400px;
-  height: 400px;
-}
-
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
-}
-
-td {
-  /*  background-color: #eee;
-  border: 1px solid #ccc; */
-  width: 60px;
-  height: 60px;
-  cursor: pointer;
-  border-radius: 10px;
-
-  &:hover {
-    background-color: rgb(165, 165, 165);
-  }
-}
-
-.sombra {
-  box-shadow: 0 0 10px 0 rgba(62, 62, 62, 0.5);
-}
-
-/* td:nth-child(odd) {
-  background-color: #ddd;
-
-  &:hover {
-    background-color: rgb(165, 165, 165);
-  }
-} */
-
 main {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
   padding: 20px;
-  background-color: rgb(247, 247, 247);
+  background-color: rgb(30, 30, 30);
+  position: fixed;
 
-  .active {
-    background-color: rgb(255, 0, 0);
-  }
+  table {
+    width: 100vw;
+    height: 100vh;
 
-  .sorter {
-    background-color: #AA09B3;
-    border-radius: 10px;
-    
-    @extend .sombra;
-  }
+    td {
+      background-color: rgba(36, 36, 36, 0.3);
+      cursor: pointer;
+      border-radius: 10px;
 
-  .order {
-    background-color: #13D7AF;
-    border-radius: 10px;
-    @extend .sombra;
+      &:hover {
+        background-color: rgb(125, 125, 125);
+      }
+    }
   }
+}
 
-  .esteira {
-    background-color: #F419FF;
-    border-radius: 10px;
-    
-    @extend .sombra;
-  }
+nav {
+  background-color: transparent;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
 
-  .rejeito {
-    background-color: #B38D12;
-    border-radius: 10px;
-    
-    @extend .sombra;
+  div {
+    width: 80%;
+    height: 100px;
+    background-color: #161616;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    box-shadow: 0 0 10px 0 rgba(62, 62, 62, 0.5);
+    border-radius: 30px 30px 0 0;
+
+    button {
+      background-color: #5312B3;
+      border: none;
+      border-radius: 10px;
+      padding: 10px 20px;
+      color: #fff;
+      font-weight: bold;
+      font-size: 16px;
+      cursor: pointer;
+      box-shadow: 0 0 10px 0 rgba(62, 62, 62, 0.5);
+    }
   }
+}
+
+
+.sombra {
+  box-shadow: 0 0 10px 0 rgba(62, 62, 62, 0.5);
+}
+
+.order {
+  background-color: #13D7AF !important;
+  border-radius: 10px;
+  @extend .sombra;
+}
+
+.esteira {
+  background-color: #F419FF;
+  border-radius: 10px;
+  @extend .sombra;
+}
+
+.rejeito {
+  background-color: #B38D12;
+  border-radius: 10px;
+  @extend .sombra;
+}
+
+.sorter {
+  background-color: #AA09B3 !important;
+  border-radius: 10px;
+  @extend .sombra;
+}
+
+.pdv {
+  background-color: #FFC500 !important;
+  border-radius: 10px;
+  @extend .sombra;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 
-createApp(App).mount('#app')
+createApp(App).use(createVuetify()).mount('#app')
